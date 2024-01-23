ffdf7bbae5966236595d1928e961fef5ab086a47
Aaron Geraci <geracia2@gmail.com>
2024-01-23 13:27:13 -0500

    create, delete and index db connections working

---
+++ backend/middleware/authentication.js
@@ -0,0 +1,13 @@
+// const jwt = require("jsonwebtoken")
+
+// async function authorize(req, res, next){
+//     try{
+//         let token = req.header("Authorization")
+//         if(!token) {
+//             return res.status(400).json({error: "No token  provided"})
+//         }
+//         console.log(token)
+//         token = token.replace("Bearer ", "")
+//     }
+
+// } 
